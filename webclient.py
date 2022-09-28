6105f9c33d9b62e826d12a124301a90aeacbc16d
Luke Reynolds <lukereynoldspi@gmail.com>
2022-09-28 08:33:11 -0700

    error message updated for lack of arguments

--- webclient.py
+++ webclient.py
@@ -5,8 +5,10 @@ if len(sys.argv) >= 3:
     host = str(sys.argv[1])
     port = int(sys.argv[2])
 else:
+    print("No host or port specified, using default values")
     host = "example.com"
     port = 80
 
 s = socket.socket()
 s.connect((host, port))
+print(s)
