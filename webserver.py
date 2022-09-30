f523e6909f488c97a26d7713c57775f138ffc9b6
Luke Reynolds <lukereynoldspi@gmail.com>
2022-09-30 11:29:10 -0700

    added comments, new error message

--- webserver.py
+++ webserver.py
@@ -1,15 +1,19 @@
 import socket
 import sys
 
+# Checks for arguments, uses port 28333 as a default
 if len(sys.argv) >= 2:
     port = int(sys.argv[1])
 else:
+    print("No host or port specified, port 28333")
     port = 28333 # Default port
 
+# Binds socket to port, socket listens
 s = socket.socket()
 s.bind(('', port))
 s.listen()
 
+# Accepts new connections, listens and decodes data
 while True:
     new_conn = s.accept()
     new_socket = new_conn[0]  # This is what we'll recv/send on
